d1602a1367187fc2bf2cf0ff866104a2e746174e
Kzawery <46087149+Kzawery@users.noreply.github.com>
2020-12-12 12:16:46 +0100

    Initial commit

---
+++ intertionalizationEmailApplication/src/main/java/com/example/validForm/Controllers/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.validForm.Controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
+
+@RequestMapping("/")
+public class HelloController {
+
+}
